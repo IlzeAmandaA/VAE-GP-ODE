ec0ec5558ae1c076652f1153da11842d848eabe3
ilze <ilze.amanda.auzina@gmail.com>
2022-11-08 16:01:49 +0100

    updated plots

--- experiments/model/misc/plot_utils.py
+++ experiments/model/misc/plot_utils.py
@@ -31,8 +31,9 @@ def plot_rollout(Xrec, show=False, fname='future.png'):
         plt.savefig(fname) 
         plt.close()
 
-def plot_rot_mnist(X, Xrec, show=False, fname='rot_mnist.png'):
-    N = min(X.shape[0],10)
+def plot_rot_mnist(X, Xrec, show=False, fname='rot_mnist.png', N=None):
+    if N is None:
+        N = min(X.shape[0],10)
     Xnp = X.detach().cpu().numpy()
     Xrecnp = Xrec.detach().cpu().numpy()
     T = X.shape[1]
@@ -46,8 +47,10 @@ def plot_rot_mnist(X, Xrec, show=False, fname='rot_mnist.png'):
             plt.subplot(2*N,T,i*T*2+t+T+1)
             plt.imshow(np.reshape(Xrecnp[i,t],[28,28]), cmap='gray')
             plt.xticks([]); plt.yticks([])
-    plt.savefig(fname)
-    if show is False:
+    if show:
+        plt.show()
+    else:
+        plt.savefig(fname)
         plt.close()
 
 
