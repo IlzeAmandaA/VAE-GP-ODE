2a841f9a2c1b31d4f23cdabfab8b3728ab9680fe
ilze <ilze.amanda.auzina@gmail.com>
2022-11-09 10:25:18 +0100

    minor updates

--- experiments/data/wrappers.py
+++ experiments/data/wrappers.py
@@ -1,21 +1,6 @@
 from .mnist import load_mnist_data
-# from .mnist_nonuniform import *
-# from .mocap_single import *
-# from .mocap_many import *
-# from .bballs import *
-
 
 def load_data(args, plot=False):
 	if args.task=='mnist':
 		trainset, testset = load_mnist_data(args, plot = plot)
-		
-	# if task=='mnist_nonuniform':
-	# 	dataset = load_mnist_nonuniform_data(data_dir,dt=dt,plot=plot)
-	# elif task=='mocap_many':
-	# 	dataset = load_mocap_data_many_walks(data_dir,dt=dt,plot=plot)
-	# elif task=='mocap_single':
-	# 	dataset = load_mocap_data_single_walk(data_dir,subject_id=subject_id,dt=dt,plot=plot)
-	# elif task=='bballs':
-	# 	dataset = load_bball_data(data_dir,dt=dt,plot=plot)
-	#[N,T,D] = trainset.shape
 	return trainset, testset #, N, T, D
